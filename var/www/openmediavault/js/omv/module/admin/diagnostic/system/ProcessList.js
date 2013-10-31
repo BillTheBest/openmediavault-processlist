791398f7b65fb04e4bc39884da4d528f329d70c7
Aaron Murray <aaron@omv-extras.org>
2013-10-31 13:15:50 -0500

    fix panel error and add locales

--- var/www/openmediavault/js/omv/module/admin/diagnostic/system/ProcessList.js
+++ var/www/openmediavault/js/omv/module/admin/diagnostic/system/ProcessList.js
@@ -1,15 +1,15 @@
 /**
  * This file is part of OpenMediaVault.
  *
- * @license		http://www.gnu.org/licenses/gpl.html GPL Version 3
- * @author		Volker Theile <volker.theile@openmediavault.org>
- * @author		Aaron Murray <aaron@omv-extras.org>
- * @author		Ian Moore <imooreyahoo@gmail.com>
- * @author		Marcel Beck <marcel.beck@mbeck.org>
- * @copyright	Copyright (c) 2009-2013 Volker Theile 
- * @copyright	Copyright (c) 2011 Ian Moore
- * @copyright	Copyright (c) 2012 Marcel Beck
- * @copyright	Copyright (c) 2013 Aaron Murray
+ * @license     http://www.gnu.org/licenses/gpl.html GPL Version 3
+ * @author      Volker Theile <volker.theile@openmediavault.org>
+ * @author      Aaron Murray <aaron@omv-extras.org>
+ * @author      Ian Moore <imooreyahoo@gmail.com>
+ * @author      Marcel Beck <marcel.beck@mbeck.org>
+ * @copyright   Copyright (c) 2009-2013 Volker Theile 
+ * @copyright   Copyright (c) 2011 Ian Moore
+ * @copyright   Copyright (c) 2012 Marcel Beck
+ * @copyright   Copyright (c) 2013 Aaron Murray
  *
  * OpenMediaVault is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,128 +26,128 @@
  */
 
 // require("js/omv/WorkspaceManager.js")
-// require("js/omv/workspace/panel/Panel.js")
+// require("js/omv/workspace/grid/Panel.js")
 // require("js/omv/data/Store.js")
 // require("js/omv/data/proxy/Rpc.js")
 
 Ext.define("OMV.module.admin.diagnostic.system.ProcessList", {
-	extend: "OMV.workspace.grid.Panel",
-	requires: [
-		"OMV.data.Store",
-		"OMV.data.proxy.Rpc"
-	],
-	
-	constructor: function(config) {
-		var me = this;
-		config = Ext.apply({
-			hideAddButton: true,
-			hideEditButton: true,
-			hideDeleteButton: true,
-			hideRefreshButton: false,
-			hidePagingToolbar: true,
-			disableSelection: true,
-			stateful: true,
-			stateId: "976130ef-a647-40e8-9b09-02ced906680a",
-			columns: [{
-					header   :_("PID"),
-					sortable :true,
-					dataIndex:"PID",
-					id       :"PID",
-					width    :60
-				},{
-					header   :_("User"),
-					sortable :true,
-					dataIndex:"USER",
-					id       :"USER",
-					width    :80
-				},{
-					header   :_("% CPU"),
-					sortable :true,
-					dataIndex:"CPU",
-					id       :"CPU",
-					width    :80
-				},{
-					header   :_("% MEM"),
-					sortable :true,
-					dataIndex:"MEM",
-					id       :"MEM",
-					width    :80
-				},{
-					header   :_("Command"),
-					sortable :true,
-					dataIndex:"COMMAND",
-					id       :"COMMAND"
-				},{
-					header   :_("State"),
-					sortable :true,
-					dataIndex:"STAT",
-					id       :"STAT",
-					renderer :function (val) {
-						/*
-						 PROCESS STATE CODES
-						 Here are the different values that the s, stat and state output specifiers
-						 (header "STAT" or "S") will display to describe the state of a process.
-						 D    Uninterruptible sleep (usually IO)
-						 R    Running or runnable (on run queue)
-						 S    Interruptible sleep (waiting for an event to complete)
-						 T    Stopped, either by a job control signal or because it is being traced.
-						 W    paging (not valid since the 2.6.xx kernel)
-						 X    dead (should never be seen)
-						 Z    Defunct ("zombie") process, terminated but not reaped by its parent.
-						 */
-						switch (val.substr(0, 1)) {
-							case 'D':
-								return _('Wait');
-							case 'R':
-								return _('Running');
-							case 'S':
-								return _('Sleeping');
-							case 'T':
-								return _('Paused');
-							case 'Z':
-								return _('Zombie');
-						}
-						return val;
-					},
-					width    :80
-				}]
-		}, config || {});
-		me.callParent([ config ]);
-	},
+    extend: "OMV.workspace.grid.Panel",
+    requires: [
+        "OMV.data.Store",
+        "OMV.data.proxy.Rpc"
+    ],
+    
+    constructor: function(config) {
+        var me = this;
+        config = Ext.apply({
+            hideAddButton: true,
+            hideEditButton: true,
+            hideDeleteButton: true,
+            hideRefreshButton: false,
+            hidePagingToolbar: true,
+            disableSelection: true,
+            stateful: true,
+            stateId: "976130ef-a647-40e8-9b09-02ced906680a",
+            columns: [{
+                    header   :_("PID"),
+                    sortable :true,
+                    dataIndex:"PID",
+                    id       :"PID",
+                    width    :60
+                },{
+                    header   :_("User"),
+                    sortable :true,
+                    dataIndex:"USER",
+                    id       :"USER",
+                    width    :80
+                },{
+                    header   :_("% CPU"),
+                    sortable :true,
+                    dataIndex:"CPU",
+                    id       :"CPU",
+                    width    :80
+                },{
+                    header   :_("% MEM"),
+                    sortable :true,
+                    dataIndex:"MEM",
+                    id       :"MEM",
+                    width    :80
+                },{
+                    header   :_("Command"),
+                    sortable :true,
+                    dataIndex:"COMMAND",
+                    id       :"COMMAND"
+                },{
+                    header   :_("State"),
+                    sortable :true,
+                    dataIndex:"STAT",
+                    id       :"STAT",
+                    renderer :function (val) {
+                        /*
+                         PROCESS STATE CODES
+                         Here are the different values that the s, stat and state output specifiers
+                         (header "STAT" or "S") will display to describe the state of a process.
+                         D    Uninterruptible sleep (usually IO)
+                         R    Running or runnable (on run queue)
+                         S    Interruptible sleep (waiting for an event to complete)
+                         T    Stopped, either by a job control signal or because it is being traced.
+                         W    paging (not valid since the 2.6.xx kernel)
+                         X    dead (should never be seen)
+                         Z    Defunct ("zombie") process, terminated but not reaped by its parent.
+                         */
+                        switch (val.substr(0, 1)) {
+                            case 'D':
+                                return _('Wait');
+                            case 'R':
+                                return _('Running');
+                            case 'S':
+                                return _('Sleeping');
+                            case 'T':
+                                return _('Paused');
+                            case 'Z':
+                                return _('Zombie');
+                        }
+                        return val;
+                    },
+                    width    :80
+                }]
+        }, config || {});
+        me.callParent([ config ]);
+    },
 
-	initComponent: function() {
-		var me = this;
-		me.store = Ext.create("OMV.data.Store", {
-			autoLoad: true,
-			fields: [
-				{ name: "PID", type: "int" },
-				{ name: "USER", type: "string" },
-				{ name: "STAT", type: "string" },
-				{ name: "CPU", type: "string" },
-				{ name: "MEM", type: "string" },
-				{ name: "COMMAND", type: "string" }
-			],
-			proxy: Ext.create("OMV.data.proxy.Rpc", {
-				appendSortParams: false,
-				rpcData: {
-					service: "ProcessList",
-					method: "getStatus"
-				}
-			}),
-			sorters: [{
-				direction: "ASC",
-				property: "PID"
-			}]
-		});
-		
-		me.callParent(arguments);
-	}
+    initComponent: function() {
+        var me = this;
+        me.store = Ext.create("OMV.data.Store", {
+            autoLoad: true,
+            fields: [
+                { name: "PID", type: "int" },
+                { name: "USER", type: "string" },
+                { name: "STAT", type: "string" },
+                { name: "CPU", type: "string" },
+                { name: "MEM", type: "string" },
+                { name: "COMMAND", type: "string" }
+            ],
+            proxy: Ext.create("OMV.data.proxy.Rpc", {
+                appendSortParams: false,
+                rpcData: {
+                    service: "ProcessList",
+                    method: "getStatus"
+                }
+            }),
+            sorters: [{
+                direction: "ASC",
+                property: "PID"
+            }]
+        });
+        
+        me.callParent(arguments);
+    }
 });
 
 OMV.WorkspaceManager.registerPanel({
-	id: "processlist",
-	path: "/diagnostic/system",
-	text: _("Process List"),
-	position: 25,
-	className: "OMV.module.admin.diagnostic.system.ProcessList"
+    id: "processlist",
+    path: "/diagnostic/system",
+    text: _("Process List"),
+    position: 25,
+    className: "OMV.module.admin.diagnostic.system.ProcessList"
 });
